75422a5e38e33ee5b47e799cecfc36d1ada0e064
danlang <tenzilangjie@gmail.com>
2016-07-28 16:57:50 +0800

    Initial Commit

---
+++ UVA-study-diary/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  UVA-study-diary
+//
+//  Created by Lang Dan on 16/7/28.
+//  Copyright © 2016年 Lang Dan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
