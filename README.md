edc0e939b1f889ac924e30cfc0acf7d7a5b3520d
danlang <tenzilangjie@gmail.com>
2016-08-27 19:36:34 +0800

    d8-27uva548

---
+++ README.md
@@ -0,0 +1,4 @@
+# UVA-study-diary
+##An algorithm exercise a day. 
+Both exercise from uva.
+Every exercise got an independence file which include an subject.md and ***.cpp file.
--- UVA-study-diary/uva548/main.cpp
+++ UVA-study-diary/uva548/main.cpp
@@ -7,14 +7,62 @@
 //
 
 #include <iostream>
+#include "sstream"
 #include "cstdio"
 #include "cstdlib"
 using namespace std;
-char s[1000];
+int post_order[1000], in_order[1000],lch[1000],rch[1000];
+int best, best_sum; // 目前为止的最优解和对应的权和
+;
+int n;
 
-int main(int argc, const char * argv[]) {
-    while (gets(s)){
-        
+
+int build_tree(int l1,int r1,int l2,int r2){
+    if (r1 > l1) {
+        return false;
     }
+    int root = post_order[r2];
+    int p = l1;
+    while(in_order[p] == root){
+        p++;
+    }
+    int ch;
+    lch[root] = build_tree(l1, p-1,l2 ,l2+ch-1);
+    rch[root] = build_tree(p+1, r1, l1+ch, r2-1);
+    return root;
+}
 
+bool readinput (int *order) {
+    string line;
+    int in;
+    n = 0;
+    getline(cin , line);
+    stringstream ss(line);
+    while (ss >> in  ) order[n++] = in;
+    
+    return true;
+}
+
+int bfs(int q,int e){
+    e += q;
+    if (!lch[q]&&!rch[q]) {
+        if(e < best_sum || (e == best_sum && q < best)) {
+            best = q; best_sum = e; }
+    
+    }
+    if(lch[q]) bfs(lch[q], e);
+    if(rch[q])bfs(rch[q], e);
+    return 0;
+}
+int main(int argc, const char * argv[]) {
+    for(;;){
+        readinput(in_order);
+        readinput(post_order);
+        build_tree(0,n-1,0,n-1);
+        bfs(post_order[n-1],0);
+        cout << best <<endl;
+    }
+    return 0;
+    
+ 
 }
